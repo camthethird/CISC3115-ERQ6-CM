cc2f0dfc2f7195e86d1d389d728e240e16f21e88
camthethird <38307166+camthethird@users.noreply.github.com>
2019-09-20 11:42:46 -0400

    Update Assignment2_Pet.java
    I couldn't get the main class to take the user inputs in vscode but it worked when I tested it on an online compiler (http://tpcg.io/pFYEMo) which let me enter the inputs. 
    
    The error I was getting in vscode from the terminal was: 
    Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
            pet cannot be resolved to a variable
            pet cannot be resolved to a variable
            pet cannot be resolved to a variable
            pet cannot be resolved to a variable
    at Pet.main(Assignment2_Part1.java:70)

--- Assignment2_Pet.java
+++ Assignment2_Pet.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 /* 
 Clifton McDonald 
 Brooklyn College 
@@ -5,27 +8,25 @@ CISC. 3115 - ERQ6
 Assignment 2 - Part 1: Pet Class
 */ 
 
+public class Pet{
 
-class Pet{
-
-    private String name;
-    private String animal;
+    private String name, animal;
     private double age;
 
 
-    // Set Pet Name
-    public void setName(String petName) {
-        name = petName;
+    // Set Name
+    public void setName(String name) {
+        this.name = name;
         }
 
     // Set animal
-        public void setAnimal(String animalName) {
-        animal = animalName;
+        public void setAnimal(String animal) {
+        this.animal = animal;
         }
 
     // Set Age
-        public void setAge(double petAge) {
-        age = petAge;    
+        public void setAge(double age) {
+        this.age = age;    
         }
 
     // Get Pet Name
@@ -38,14 +39,32 @@ class Pet{
         return animal;
         }
 
-    
     // Get Age
         public double getAge() {
         return age;
         }
 
         public static void main(String[] args) {
+    
+            Scanner sc = new Scanner(System.in);
+            Pet p = new Pet();    
+            try{
+                System.out.println("Please enter your pets name: ");
+                p.setName(sc.nextLine());
+                System.out.println("Please enter the kind of animal your pet is: ");
+                p.setAnimal(sc.nextLine());
+                System.out.println("Please enter your pets age: ");
+                p.setAge(sc.nextDouble());
+            } catch(Exception e){ System.out.println("I/O Exception");}
         
-        }    
+        
+            System.out.println("Your pet's name is: " + p.getName());
+            System.out.println(p.getName() + " is a " + p.getAnimal());
+            System.out.println(p.getName() + " is " + p.getAge() + " years old");
+        
+            sc.close();
+        
+            }  
 
-} //Close Pet Class   
+}// close Pet Class
+   
