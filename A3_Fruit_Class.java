bbe25a78788e777f82bacb401fac85e3431fc6ae
Clifton McDonald <38307166+camthethird@users.noreply.github.com>
2019-10-04 16:43:53 -0400

    Add files via upload
    WIP.

---
+++ A3_Fruit_Class.java
@@ -0,0 +1,38 @@
+/*
+--------------------------------------  |
+Clifton McDonald                        |
+CUNY - Brooklyn College                 | 
+CISC. 3115-ERQ6                         |
+Assignmet 3 - Part 1 : "Fruit Class"    |    
+--------------------------------------  |
+*/
+
+class Fruit {
+
+    String name;
+    String taste; 
+    String size; 
+
+
+    static void eat(){
+
+       //filler
+       System.out.println("Delecious!");
+        
+    }
+}// Closes Fruit Class
+
+class Apple extends Fruit {
+
+    private String name = "Apple";
+
+    public static void main(String[] args){
+
+        //filler
+    
+    }
+
+
+} //Closes Apple ext. 
+
+
