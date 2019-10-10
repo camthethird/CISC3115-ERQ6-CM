993642c5957330575c1030857bd1063d3f8e8cc1
Clifton McDonald <38307166+camthethird@users.noreply.github.com>
2019-10-10 16:27:03 -0400

    Update A3_Fruit_Class.java
    Completed.

--- A3_Fruit_Class.java
+++ A3_Fruit_Class.java
@@ -7,41 +7,68 @@ Assignmet 3 - Part 1 : "Fruit Class"    \
 --------------------------------------  /
 */
 
+/*
+--------------------------------------  \
+Clifton McDonald                        /
+CUNY - Brooklyn College                 \ 
+CISC. 3115-ERQ6                         /
+Assignmet 3 - Part 1 : "Fruit Class"    \    
+--------------------------------------  /
+*/
+
+//import java.util.*;
+
 class Fruit {
 
     //Attributes
-   protected String name = "";
-   protected String taste = ""; 
-   protected String size = ""; 
+   protected String name = "fruit";
+   protected String taste = "taste"; 
+   protected String size = "size"; 
 
+    public void eat(){
 
-    static void eat(){
-
-       //filler
-       System.out.println("Delicious!");
-        
-    }
+            System.out.println("Fruit: " + name);
+            System.out.println("Taste: " + taste);
+            System.out.println("You're eating an " + name + " that tastes " + taste + "."); 
+     }
+     
 }// Closes Fruit Class
 
     //Apple ext.
     class Apple extends Fruit {
-
-         private String name = "Apple";
-
-             public static void main(String[] args){
-
-            //filler
-    
-            }
-
+        
+        public Apple(){
+            name = "Apple";
+            taste = "Sweet";
+            size = "big";
+        }
 
     } //Closes Apple ext. 
 
     //Orange ext. 
     class Orange extends Fruit {
 
-        private String name = "Orange";
+        public Orange(){
+
+            name = "Orange";
+            taste = "Tangy";
+            size = "small";
+
+        }
 
         
     }//Closes Orange ext. 
 
+
+    //Main Class/ Fruit Driver
+public class Main {   
+    public static void main(String[] args) {
+        Apple redApple = new Apple();
+        redApple.eat();
+      
+
+        Orange jaffaOrange = new Orange();
+        jaffaOrange.eat();
+      
+    }
+ } 
